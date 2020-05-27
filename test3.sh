8f854c104888c8cd6b8ac720ae4b2df503c5e739
aezquerro <albertoezquerro@gmail.com>
2020-05-27 13:39:34 +0000

    9

--- test3.sh
+++ test3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 rostopic pub -1 /ardrone/takeoff std_msgs/Empty;
-rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.1, y: 0.1, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 18 ; kill $!
-rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.0, z: 0.1}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 10 ; kill $!
-rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: -0.1, y: -0.1, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 18 ; kill $!
-rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.0, z: -0.1}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 10 ; kill $!
-rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.1, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 13 ; kill $!
-rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: -0.1, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0,z: 0.0}}' & sleep 26 ; kill $!
-rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: -0.1, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 13 ; kill $!
+rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.2, y: 0.2, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 18 ; kill $!
+rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.0, z: 0.2}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 10 ; kill $!
+rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.2, y: -0.2, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 18 ; kill $!
+rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.0, z: -0.2}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 10 ; kill $!
+rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.2, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 13 ; kill $!
+rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: -0.2, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0,z: 0.0}}' & sleep 26 ; kill $!
+rostopic pub -r 10 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: -0.2, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}' & sleep 13 ; kill $!
 rostopic pub -1 /ardrone/land std_msgs/Empty;
 
